f14a1f8ddc004ff79e9c03a227b1c29f87ccdffc
devshum <57689318+devshum@users.noreply.github.com>
2022-01-20 17:05:46 +0200

    http-service

---
+++ src/app/core/services/http.service.ts
@@ -0,0 +1,37 @@
+import { environment } from './../../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  private _apiUrl = environment.apiUrl;
+  constructor(private _http: HttpClient) { }
+
+  getGames(ordering: string, search?: string): Observable<GameResponse> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      'x-rapidapi-key': 'ff865bb1dfmsh9ab225d644e14f1p172b82jsn8689cb5dd275'
+    });
+
+    let options = {
+      params: new HttpParams()
+      .set('ordering', ordering),
+      headers
+    };
+
+    if(search) {
+      options = {
+        params: new HttpParams()
+        .set('ordering', ordering)
+        .set('search', search),
+        headers
+      };
+    }
+
+    return this._http.get<GameResponse>(`${this._apiUrl}/games`, options);
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://rawg-video-games-database.p.rapidapi.com/games/%7Bgame_pk%7D'
 };
 
 /*

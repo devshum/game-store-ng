22ebf6c3f7ed75da7123ffc378ab45e1e003dc79
devshum <57689318+devshum@users.noreply.github.com>
2022-01-31 14:03:06 +0200

    redesign-card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Game } from 'src/app/core/models/game.interface';
+import { GamesService } from 'src/app/core/services/games.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent  {
+export class CardComponent implements OnInit {
   @Input() game: Game;
-  constructor(private _router: Router) { }
+  gameSub: Subscription;
+  gameRating = 0;
+  constructor(
+    private _router: Router,
+    private _gamesService: GamesService
+  ) { }
+  ngOnInit(): void {
+    this.getGameDetails(this.game.id);
+  }
+
+  getGameDetails(id: string): void {
+    this.gameSub = this._gamesService.getGameDetails(id).subscribe((data: Game) => {
+      this.game = data;
+      this.gameRating = this.game.metacritic;
+    });
+  }
 
   openGameDetails(id: string): void {
     this._router.navigate(['details', id]);
--- src/app/core/models/game-response.interface.ts
+++ src/app/core/models/game-response.interface.ts
@@ -3,5 +3,6 @@ import { Game } from './game.interface';
 export interface GameResponse {
   results: Game[];
   count: number;
+  seo_title: string;
 }
 

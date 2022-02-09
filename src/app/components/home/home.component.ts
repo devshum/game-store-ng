a6847418f646410cff7f4550366913fb000e5989
devshum <57689318+devshum@users.noreply.github.com>
2022-02-09 18:38:36 +0200

    loader-service

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { LoaderService } from './../../core/services/loader.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { simpleFilterOptions } from 'src/app/core/models/filters.interface';
 import { SelectItem } from 'primeng/api';
@@ -18,18 +19,23 @@ export class HomeComponent implements OnInit, OnDestroy {
   public count: number;
   public title: string;
   public pageSize = 21;
+  public load = true;
+  public skeletons = [...Array(this.pageSize)].map((_, i) => i + 1);
 
   private _routeSub: Subscription;
   private _gameSub: Subscription;
 
   constructor(
     private _gamesService: GamesService,
-    private _activetedRoute: ActivatedRoute
+    private _activetedRoute: ActivatedRoute,
+    private _loaderService: LoaderService
   ) {
     this.parameters = simpleFilterOptions;
   }
 
   ngOnInit(): void {
+    this._loaderService.loadingStatus.subscribe((isLoad: boolean) => this.load = isLoad);
+
     this._routeSub = this._activetedRoute.params.subscribe((params: Params) => {
       if(params['game-search']) {
         this.searchGames(this.selectedParamValue, this.pageSize, params['game-search']);
@@ -40,12 +46,14 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   searchGames(sort: string, pageSize: number, search?: string): void {
+    this._loaderService.start();
     this._gameSub = this._gamesService
     .getGames(sort, pageSize, search)
     .subscribe(data => {
       this.games = data.results;
       this.count = data.count;
       this.title = data.seo_title;
+      this._loaderService.end();
     });
   }
 

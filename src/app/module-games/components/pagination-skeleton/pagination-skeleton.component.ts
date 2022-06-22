69153b5a9633b55b2f6bd0dca3ebbabc9e468e3e
devshum <57689318+devshum@users.noreply.github.com>
2022-06-22 17:53:56 +0300

    pagination skeleton was added

---
+++ src/app/module-games/components/pagination-skeleton/pagination-skeleton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination-skeleton',
+  templateUrl: './pagination-skeleton.component.html',
+  styleUrls: ['./pagination-skeleton.component.scss']
+})
+export class PaginationSkeletonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

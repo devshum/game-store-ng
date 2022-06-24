b3cc3461f4a6c1a6f90649041d227083454ca842
devshum <57689318+devshum@users.noreply.github.com>
2022-06-24 16:49:52 +0300

    app routing was updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
-import { GameDetailsComponent } from './module-game-details/components/game-details/game-details.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
@@ -8,16 +7,16 @@ const routes: Routes = [
     pathMatch: 'full',
     redirectTo: 'games'
   },
-  // {
-  //   path: 'search/:game-search',
-  //   { path: 'articles/:id', loadChildren: () => import('./module-game-details/game-details.module').then(m => m.GameDetailsModule) },
-  // },
   { path: 'games', loadChildren: () => import('./module-games/games.module').then(m => m.GamesModule) },
-  { path: 'articles/:id', loadChildren: () => import('./module-game-details/game-details.module').then(m => m.GameDetailsModule) },
+  { path: 'games/:game-search', loadChildren: () => import('./module-games/games.module').then(m => m.GamesModule) },
+  { path: 'details/:id', loadChildren: () => import('./module-game-details/game-details.module').then(m => m.GameDetailsModule) },
+  { path: '**',
+    redirectTo: 'games'
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled', preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

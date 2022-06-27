e1561efca38ac147e21e61e86dbd6bd2b864913d
devshum <57689318+devshum@users.noreply.github.com>
2022-06-27 18:46:34 +0300

    game details skeleton was added

---
+++ src/app/module-game-details/components/game-details-skeleton/game-details-skeleton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-details-skeleton',
+  templateUrl: './game-details-skeleton.component.html',
+  styleUrls: ['./game-details-skeleton.component.scss']
+})
+export class GameDetailsSkeletonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

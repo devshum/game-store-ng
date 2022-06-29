61ecc1d7087ad8e9c03fa7dfb14a59241876aed3
devshum <57689318+devshum@users.noreply.github.com>
2022-06-29 10:31:10 +0300

    game details was refactored

--- src/app/module-game-details/components/game-details/game-details.component.ts
+++ src/app/module-game-details/components/game-details/game-details.component.ts
@@ -1,53 +1,26 @@
-import { GamesService } from 'src/app/core/services/games.service';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { pluck } from 'rxjs/operators' ;
 import { Game } from 'src/app/core/models/game.interface';
+
 import SwiperCore, { FreeMode, Navigation, Thumbs } from 'swiper';
-import { LoaderService } from 'src/app/core/services/loader.service';
-import { takeUntil } from 'rxjs/operators';
 SwiperCore.use([FreeMode, Navigation, Thumbs]);
+
+import * as fromApp from '../../../core/models/store.interface';
 
 @Component({
   selector: 'app-game-details',
-  templateUrl: './game-details.component.html',
-  styleUrls: ['./game-details.component.scss']
+  templateUrl: './game-details.component.html'
 })
-export class GameDetailsComponent implements OnInit, OnDestroy {
-  public game: Game;
-  public load = true;
-
-  private _gameId: string;
-  private _unsubscribe = new Subject();
+export class GameDetailsComponent implements OnInit {
+  public game$: Observable<Game | null>;
 
   constructor(
-    private _activatedRoute: ActivatedRoute,
-    private _gamesService: GamesService,
-    private _loaderService: LoaderService
+    private _store: Store<fromApp.AppState>
   ) {}
 
   ngOnInit(): void {
-      this._loaderService.loadingStatus.pipe(takeUntil(this._unsubscribe))
-                                       .subscribe((isLoad: boolean) => this.load = isLoad);
-
-      this._activatedRoute.params.pipe(takeUntil(this._unsubscribe))
-                                                .subscribe((params: Params) => {
-      this._gameId = params.id;
-      this._getGameDetails(this._gameId);
-    });
-  }
-
-  ngOnDestroy(): void {
-    this._unsubscribe.next();
-  }
-
-  private _getGameDetails(id: string): void {
-    this._loaderService.start();
-    this._gamesService.getGameDetails(id)
-                      .pipe(takeUntil(this._unsubscribe))
-                      .subscribe((data: Game) => {
-      this.game = data;
-      this._loaderService.end();
-    });
+    this.game$ = this._store.select('gameDetails').pipe(pluck('game'));
   }
 }
